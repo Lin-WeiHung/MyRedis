2c7a916f5b47855e68a61b7d99e330704fc862f8
Lin-WeiHung <bb232399@gmail.com>
2023-11-07 18:33:03 +0800

    first commit

---
+++ redis.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<time.h>
+#include "../redis-1.3.6/dict.c"
+#include "../redis-1.3.6/zmalloc.c"
+
+int main(){
+    unsigned int hs;
+    hs = 1;
+    printf("%u\n", hs);
+}
